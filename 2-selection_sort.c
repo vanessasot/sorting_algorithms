e1e431f5c5258995722c3f718f14b54119f2fc4a
Vanessa <vannexxaa@gmail.com>
2020-10-20 01:01:32 +0000

    Update file betty

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,27 +8,27 @@
  */
 void selection_sort(int *array, size_t size)
 {
-        size_t i, j, min;
-        int aux;
+	size_t i, j, min;
+	int aux;
 
-        if (array == NULL || size < 2)
-                return;
-        for (i = 0; i < size - 1; i++)
-        {
-                min = i;
-                for (j = i + 1; j < size; j++)
-                {
-                        if (array[min] > array[j])
-                        {
-                                min = j;
-                        }
-                }
-                if (min != i)
-                {
-                        aux = array[min];
-                        array[min] = array[i];
-                        array[i] = aux;
-                        print_array(array, size);
-                }
-        }
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			aux = array[min];
+			array[min] = array[i];
+			array[i] = aux;
+			print_array(array, size);
+		}
+	}
 }
