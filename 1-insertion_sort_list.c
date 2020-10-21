ecb002bb788a4ede4def4061fe307614b8a650f0
Andrescortes23 <andresccortes23@icloud.com>
2020-10-21 16:38:16 +0000

    Task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ *insertion_sort_list - sort double linked list with insert algorithm
+ *@list: double linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp = NULL;
+
+	tmp = (*list)->next;
+
+	while (tmp != NULL)
+	{
+		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->prev->prev;
+			tmp->next->prev = tmp;
+
+			if (tmp->prev == NULL)
+				*list = tmp;
+			else
+				tmp->prev->next = tmp;
+			print_list(*list);
+		}
+		tmp = tmp->next;
+	}
+}
