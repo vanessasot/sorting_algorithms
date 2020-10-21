97e5bf4056027d11eb99d0188dce40de35035f46
Vanessa <vannexxaa@gmail.com>
2020-10-21 22:20:50 +0000

    Add prototypes task 3

--- sort.h
+++ sort.h
@@ -17,11 +17,15 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Prototypes */
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+int divisions(int *array, int begin, int end, size_t size);
+void quick_sort_rec(int *array, int begin, int end, size_t size);
+void quick_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
